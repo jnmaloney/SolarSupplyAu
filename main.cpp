f9e43fa2ef70c018eaf1ef18d701367e1ff73429
jnmaloney <jnmaloney@gmail.com>
2020-05-25 11:39:10 +1000

    Delete main.cpp

--- main.cpp
+++
@@ -1,5 +0,0 @@
-// Abstract: SolarSupplyAu is a data processing and visualisation system for exploring daily solar supply in Australian states and suburbs. The data processing unit takes input data about the current number of solar photovoltaic system installations per postcode, and estimates the performance daily based on weather reports. The visualisation unit allows the exploration of the dauily statistics from the national level down to each state and postcode. Postcode area visualisations are enhanced with a 3D rendering of the landscape and a representaion of the percentage of residences contributing to the solar supply. 
-int main()
-{
-  return 0;
-}
